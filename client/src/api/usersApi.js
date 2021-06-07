f3978b6afa01830e91c8adb149a92b14bbe021d8
zeyadShapan <zeyadshapan2004@gmail.com>
2021-06-07 18:48:55 +0200

    Fixed apiUrl

--- client/src/api/usersApi.js
+++ client/src/api/usersApi.js
@@ -1,9 +1,8 @@
 import http from './http';
+import config from '../config'
 
-const apiUrl = `http://localhost:5000/api`;
-
-const createUser = data => http.post(`${apiUrl}/users`, data)
-const loginUser = data => http.post(`${apiUrl}/auth`, data)
+const createUser = data => http.post(`${config.apiUrl}/users`, data)
+const loginUser = data => http.post(`${config.apiUrl}/auth`, data)
 
 const usersApi = {createUser, loginUser}
 export default usersApi
