f1fdf4996df51647d03248fcb1191b82be063c4a
zeyadShapan <zeyadshapan2004@gmail.com>
2021-06-07 18:17:28 +0200

    Just testing

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -7,6 +7,9 @@ const deleteToken = () => localStorage.removeItem(key);
 const getToken = () => localStorage.getItem(key);
 const getUser = () => {
     const token = getToken()
+    console.log('token:',  token)
+    console.log('jwtdecode:',  jwtDecode)
+    console.log('jwtdecode(token):',  jwtDecode(token))
     return token && jwtDecode(token)
 }
 
